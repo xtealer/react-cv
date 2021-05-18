34749928bd1f4aedd842bb24413cf957bdb79df5
Enrique Shunnar <xtealer00@gmail.com>
2021-05-17 21:15:48 -0500

    Add logout functionality

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -1,11 +1,34 @@
-import React from "react";
-import { Menu } from "antd";
+import React, { useCallback } from "react";
+import { Menu, message } from "antd";
 import { Link, useHistory, useLocation } from "react-router-dom";
-import { useCallback } from "react";
+import useCurrentUserHook from "../hooks/useCurrentUserHook";
+
+import firebase from "firebase/app";
+import "firebase/auth";
+import LogoutOutlined from "@ant-design/icons/lib/icons/LogoutOutlined";
+
+const logoutIconStyle = { color: "red" };
 
 const NavbarComponent = () => {
+  const user = useCurrentUserHook();
   const location = useLocation();
   const history = useHistory();
+
+  const doLogout = useCallback(async () => {
+    try {
+      await firebase.auth().signOut();
+      message.info("Session Closed.");
+    } catch (e) {
+      console.log(e);
+      message.error("Something failed");
+    }
+
+    if (location.pathname !== "/") {
+      setTimeout(() => {
+        history.replace("/");
+      }, 1500);
+    }
+  }, []);
 
   const scrollToContactForm = useCallback(() => {
     new Promise((resolve) => {
@@ -27,9 +50,16 @@ const NavbarComponent = () => {
       <Menu.Item key="/#contact" onClick={scrollToContactForm}>
         Contact
       </Menu.Item>
-      <Menu.Item key="/login">
-        <Link to="/login">Login</Link>
-      </Menu.Item>
+
+      {!user ? (
+        <Menu.Item key="/login">
+          <Link to="/login">Login</Link>
+        </Menu.Item>
+      ) : (
+        <Menu.Item key="logout" onClick={doLogout}>
+          <LogoutOutlined style={logoutIconStyle} />
+        </Menu.Item>
+      )}
     </Menu>
   );
 };
